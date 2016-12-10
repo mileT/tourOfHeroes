ad29d6772bd5527e7ee87ff463e6c69e55c434e4
Jianan Han <jianan.h@gmail.com>
2016-12-10 18:41:10 -0500

    add hero list to component

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,19 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   template: `
@@ -15,6 +28,13 @@ export class Hero {
       <label>name: </label>
       <input [(ngModel)]="hero.name" placeholder="name">
     </div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+        <!-- each hero goes here -->
+      </li>
+    </ul>
     `
 })
 export class AppComponent  {
@@ -23,4 +43,5 @@ export class AppComponent  {
     id: 1,
     name: 'Windstorm'
   };
+  heroes = HEROES;
 }
