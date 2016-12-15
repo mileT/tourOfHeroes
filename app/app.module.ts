875b754b2e1df373390e2610dd4ed5747c9538d8
Michael Han <jianan.h@gmail.com>
2016-12-15 08:34:55 -0500

    update http promise

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading and configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory web api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -8,7 +15,7 @@ import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 
-import { AppRoutingModule } from './app-routing.module';
+
 
 
 
@@ -16,7 +23,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
     ],
   declarations: [
      AppComponent,
